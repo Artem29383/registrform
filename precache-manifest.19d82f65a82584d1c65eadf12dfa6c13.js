95be575924b360f2206d9aef86713222037499e2
Artem <artyom.aweryanow@yandex.ru>
2019-11-24 01:51:04 +0300

    Updates

---
+++ precache-manifest.19d82f65a82584d1c65eadf12dfa6c13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c62cd0ceea933ba884122a00d881653",
+    "url": "/registrform/index.html"
+  },
+  {
+    "revision": "ef70b3109b42d9191157",
+    "url": "/registrform/static/css/main.30056fd6.chunk.css"
+  },
+  {
+    "revision": "451f5b59a8dbe47b7b17",
+    "url": "/registrform/static/js/2.df00ca59.chunk.js"
+  },
+  {
+    "revision": "ef70b3109b42d9191157",
+    "url": "/registrform/static/js/main.ff4225da.chunk.js"
+  },
+  {
+    "revision": "738582702fbb5b85f24d",
+    "url": "/registrform/static/js/runtime-main.e4f1f960.js"
+  }
+]);
